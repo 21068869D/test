c2a0c90d6e912a124c6727c806421096e1260ddb
Bernard Lau <21035849d@connect.polyu.hk>
2022-04-04 22:09:19 +0800

    Header, footer initial commit

---
+++ footer.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<html>
+    <body>
+        <footer class="footer">
+            <div class="social">
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-youtube"></i></a>
+                <a href="#"><i class="fab fa-discord"></i></a>
+                
+            </div>
+            <ul class="footer-list">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Browse NFTs</a></li>
+                <li><a href="#">Help</a></li>
+                <li><a href="#">Terms of service</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+            </ul>
+        </footer>
+    </body>
+</html>
---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css" type="text/css">
+
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <img src="assets/logo.png" alt="logo" class="logo">
+                <nav>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Browse</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Login</a></li>
+                        <li><a href="#">Signup</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+
+    </body>
+</html>
