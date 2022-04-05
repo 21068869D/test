b2893f1bd4dcebb7d4c7778221e0f55fce90c9c0
Alex Mui <muitinyau@gmail.com>
2022-04-05 23:03:16 +0800

    Co-authored-by: himng25 <himng25@users.noreply.github.com>

--- login.php
+++ login.php
@@ -5,6 +5,67 @@ include_once 'header.php';
     <head>
         <link rel="stylesheet" href="style.css" type="text/css">
     </head>
+
+    <body style="background-color: rgb(32, 34, 37);">
+        <div class="parallax-3">
+            <div class="space"></div>
+            <section  class="signupform">
+                <h2>Login</h2>
+                <p>Please fill in your credentials to login.</p>
+
+                <?php
+                /*
+                if (!empty($login_err)) {
+                    echo '<div class="alert alert-danger">' . $login_err . '</div>';
+                }*/
+                ?>
+
+                <form action="" method="post">
+                    <div class="signup-item">
+                        <label>Username</label>
+                        <input type="text" name="username" class="form-control ">
+                        <span class="invalid-feedback"></span>
+                    </div>
+                    <div class="signup-item">
+                        <label>Password</label>
+                        <input type="password" name="password" class="form-control ">
+                        <span class="invalid-feedback"></span>
+                    </div>
+                    <div class="signup-item">
+                        <div>
+                            <input type="checkbox" value="Remember me" name="remember_me_checkbox" />
+                            <label for="remember_me_checkbox">Remember Me</label>
+                            <br><br>
+                        </div>
+                        <input type="submit" class="signup-btn" value="Login">
+                    </div>
+
+                    <div class="notroboot-item">
+                        <div>
+                            <?php
+                            /*
+                                require_once('recaptchalib.php');
+                                $publickey = "your_public_key"; // you got this from the signup page
+                                echo recaptcha_get_html($publickey);*/
+                            ?>
+
+
+
+                        
+                           
+                        </div>
+                    </div>
+
+                    <p>Haven’t register yet? <a href="register.php">Register your account</a></p>
+                </form>
+            </section>
+            
+        </div>
+            
+            
+        
+    </body>
+<!--     
     <body  style="background-color: rgb(32, 34, 37);">
         <div class="parallax-3">
             <div class="space"></div>
@@ -21,17 +82,10 @@ include_once 'header.php';
                             </div>
                             <button class="btn-login" type="submit" name="submit">Log in</button>
                         </form>
-                        <?php
-                            if(isset($_GET["error"])){
-                            if($_GET["error"] == "loginfailed"){
-                                echo "<p>Incorrect username/password.</p>";
-                            }
-                        }
-                    
-                    ?>
+                       
                     </section>
             <div class="space"></div>
         </div>
 
-    </body>
+    </body> -->
 </html>
