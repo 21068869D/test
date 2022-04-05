933efcf8c59b76e0e0483216c9dc58a764fd4c5b
Walter <21068869D@connect.polyu.hk>
2022-04-05 22:45:46 +0800

    Walter

---
+++ createNFT.php
@@ -0,0 +1,30 @@
+
+<?php
+include_once 'header.php';
+?>
+
+<div class="space"></div>
+    <section class="imageform">
+            <h2>Post new NFT</h2>
+           
+            <form action="inc/create.inc.php" method="post">
+                <div class="image-item">
+                    <label for="imagetitle">NFT title: </label>
+                    <input type="text" name="imagetitle" placeholder="NFT title" required>
+                </div>
+                <div class="image-item">
+                    <label for="price">Price: </label>
+                    <input type="text" name="price" placeholder="Price" required>
+                </div>
+                <div class="image-item">
+                    <label for="image">Please upload NFT image:</label>
+                    <input type="file" name="profileimg">
+                </div>
+                <?php if(isset($_SESSION["useruid"])){
+                    echo "<input type='hidden' name='useruid' value='". $_SESSION["useruid"] ."'";} ?>
+                
+                <button type="reset" name="reset">Reset</button>
+                <button type="submit" name="create">Create</button>
+            </form>
+            <div class="space"></div>
+</html>
