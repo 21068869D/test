2b2c19f6cd88e9a10d2f3dcb2ca150a5e5098abc
rexluk <94114068+rexluk@users.noreply.github.com>
2022-04-06 17:27:40 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body, html {
+  height: 100%;
+}
+
+.img1 {
+  /* The image used */
+  background-image: url('https://thumbs.dreamstime.com/z/nft-non-fungible-token-crypto-art-concept-215531358.jpg');
+
+  /* Full height */
+  height: 100%; 
+
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.img2 {
+  /* The image used */
+  background-image: url('https://thumbs.dreamstime.com/z/nft-non-fungible-token-symbol-screen-smartphone-background-beeple-digital-art-everydays-first-days-kazan-russia-april-215041851.jpg');
+
+  /* Full height */
+  height: 100%; 
+
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.img3 {
+  /* The image used */
+  background-image: url('https://thumbs.dreamstime.com/z/concept-human-interaction-technologies-virtual-world-to-achieve-material-moral-well-being-nft-art-bidding-241221663.jpg');
+
+  /* Full height */
+  height: 100%; 
+
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
+</head>
+<body>
+
+<div class="img1"></div>
+
+<div style="height:1000px;background-color:blue;font-size:36px">
+In this website,you can buy or sell the NFT art. 
+</div>
+
+<div class="img2"></div>
+
+<div style="height:1000px;background-color:yellow;font-size:36px">
+You can using any payment method for buy and sell the NFT art.
+</div>
+
+<div class="img3"></div>
+
+<div style="height:50px;background-color:grey;font-size:36px">
+<a href='./login'>Click here to login the system.</a>
+</div>
+
+</body>
+</html>
