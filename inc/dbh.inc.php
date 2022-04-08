f5895755533625b366be967f1ffc19dcf75c5e4a
rexluk <94114068+rexluk@users.noreply.github.com>
2022-04-08 17:28:19 +0800

    Update dbh.inc.php

--- inc/dbh.inc.php
+++ inc/dbh.inc.php
@@ -1,12 +1,18 @@
 <?php
+require_once 'config.php';
 
-$serverName = "localhost";
-$dBUsername = "root";
-$dBPassword = "";
-$dBName = "security_project";
+$dsn = "mysql:host=$host;dbname=$db;charset=UTF8";
 
-$conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
+function dbconnect() {
+  try {
+    global $dsn, $dbuser, $dbpwd;
+    $pdo = new PDO($dsn, $dbuser, $dbpwd);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,
+                     PDO::ERRMODE_EXCEPTION);
+    return $pdo;
+  } catch (PDOException $e) {
+    die('Database Error');
+  }
+}
 
-if(!$conn){
-    die("Cannot connect to server, Reason: " . mysqli_connect_error());
 }
