803e4e5aca810fbde398e30ff60b30ace70a4d58
rexluk <94114068+rexluk@users.noreply.github.com>
2022-04-08 19:17:33 +0800

    Update dbh.inc.php

--- inc/dbh.inc.php
+++ inc/dbh.inc.php
@@ -1,8 +1,11 @@
 <?php
 require_once (./config.php);
 
-$conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
 
-if(!$conn){
+try {
+$conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
+}catch{
     die("Cannot connect to server, Reason: " . mysqli_connect_error());
 }
+
+?>
