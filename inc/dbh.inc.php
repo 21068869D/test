6fa70234a26720bc862f9a89c21c6e0725eb0319
rexluk <94114068+rexluk@users.noreply.github.com>
2022-04-08 16:21:08 +0800

    Update dbh.inc.php

--- inc/dbh.inc.php
+++ inc/dbh.inc.php
@@ -3,7 +3,7 @@
 $serverName = "localhost";
 $dBUsername = "root";
 $dBPassword = "";
-$dBName = "integratedproject";
+$dBName = "security_project";
 
 $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
 
