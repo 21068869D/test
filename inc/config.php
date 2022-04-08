ce1812fca44b4a6face27dda0d12dcfd4b35e7b6
rexluk <94114068+rexluk@users.noreply.github.com>
2022-04-08 16:23:33 +0800

    Create config.php

---
+++ inc/config.php
@@ -0,0 +1,8 @@
+<?php
+
+$serverName = "localhost";
+$dBUsername = "security_project_admin";
+$dBPassword = "47ev5HbFoMt8Bp";  //just random string,can use or change before demo
+$dBName = "security_project";
+
+?>
