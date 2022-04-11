507522a1acc1509567d40dacc680f53078a61599
rexluk <94114068+rexluk@users.noreply.github.com>
2022-04-12 04:54:11 +0800

    Update register.inc.php

--- inc/register.inc.php
+++ inc/register.inc.php
@@ -2,23 +2,32 @@
 require_once 'dbh.inc.php';
 require_once 'functions.inc.php';
 
-
-function upload($profile_image){
-if(isset($_POST["submit"])){
-    /*$email = $_POST["email"];
+function upload_img($profile_image){
+    /*
+	$email = $_POST["email"];
     $username = $_POST["uid"];
     $pwd = $_POST["password"];
-    $repeatpwd = $_POST["confirmpwd"];*/
+    $repeatpwd = $_POST["confirmpwd"]; */
     
-    $profileImageName = time() . '-' .$profile_image["tmp_name"];
-    $target_dir = "../upload/";
-    $move_file = "../upload/" . $profileImageName;
-    $target_file = $target_dir . basename($profileImageName);
-    
-    //print_r($_FILES["profile_image"]);
-    move_uploaded_file( $profile_image["tmp_name"],$move_file);
-    return $move_file;
+	//print_r($profile_image["tmp_name"]);
+    $profileImageName = time().'-'.$profile_image["name"];
+    $target_dir = $_SERVER['DOCUMENT_ROOT']."/test/upload/";
+    $move_file = $_SERVER['DOCUMENT_ROOT']."/test/upload/" . basename($profileImageName);
+    //$target_file = $target_dir . basename($profileImageName);
+	//echo($_SERVER['DOCUMENT_ROOT']."/upload");
+    //print("<br/>".$move_file);
+	if(is_writable($target_dir){
+		move_uploaded_file($profile_image["tmp_name"],$move_file);
+		return $profile_image;
+	}else{
+	return "The directory cannot write.";}
+}
+
 /*
+	$email = $_POST["email"];
+    $username = $_POST["uid"];
+    $pwd = $_POST["password"];
+    $repeatpwd = $_POST["confirmpwd"]; 
     if(pwdMatch($pwd, $repeatpwd)!==false){
         header("location: ../signup.php?error=passwordmismatch");
         exit();
@@ -36,5 +45,4 @@ else{
 }
 
 */
-}
-}
+?>
