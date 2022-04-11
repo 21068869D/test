f1336b1d88f91034fa5a398e9b25768d6220118c
Bernard Lau <21035849d@connect.polyu.hk>
2022-04-11 11:46:03 +0800

    return to og code for testing

--- inc/dbh.inc.php
+++ inc/dbh.inc.php
@@ -1,11 +1,21 @@
 <?php
-require_once (./config.php);
+require_once ('./config.php');
 
-
-try {
 $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
-}catch{
+
+if(!$conn){
     die("Cannot connect to server, Reason: " . mysqli_connect_error());
 }
+
+
+/*try {
+$conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
+}catch(){
+    die("Cannot connect to server, Reason: " . mysqli_connect_error());
+}
+^@rexluk Didnt work so I comment them for now - Bernard
+*/
+
+
 
 ?>
